3b0390b6c3766c0763e7444b9966d468a4e19000
Elam Cohavi <elam.cohavi@gmail.com>
2022-12-01 14:44:53 +0000

    day 1

---
+++ 01/puzzle.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func parse() ([]int, error) {
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal("reading file", err)
+	}
+
+	var o []int
+	for _, v := range strings.Split(string(input), "\n\n") {
+		sum := 0
+		for _, s := range strings.Split(v, "\n") {
+			conv, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			sum += conv
+		}
+		o = append(o, sum)
+	}
+	return o, nil
+}
+
+func One(data []int) {
+	sort.Ints(data)
+	fmt.Println(data[len(data)-1])
+}
+
+func Two(data []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(data)))
+	sum := 0
+	for _, v := range data[:3] {
+		sum += v
+	}
+	fmt.Println(sum)
+}
+
+func main() {
+	data, err := parse()
+	if err != nil {
+		panic(err)
+	}
+
+	One(data)
+	Two(data)
+}
